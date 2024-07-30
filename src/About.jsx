a3da9d533826583747311d0e78d851884f30389b
Maximilian Schulten <maximilian.schulten1@gmail.com>
2024-07-30 14:56:36 -0400

    Adding major changes, finished basically everything now displays github repos from github REST API ready to publish.

--- src/About.jsx
+++ src/About.jsx
@@ -1,36 +1,44 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Card, CardBody, CardHeader, Divider, Link } from "@nextui-org/react"
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 
 function About() {
     return (
         <>
             <h1 className='text-white mt-10 text-3xl text-center'>About me.</h1>
-            <div className="m-10 gap-10 grid grid-cols-4 content-center">
+            <div className="m-10 gap-10 grid lg:grid-cols-2 sm:grid-cols-1 content-center">
+                <div className="grid grid-cols-1 gap-3 content-center grid-rows-2">
+                    <Card className="bg-black/40 h-fit">
+                        <CardHeader className="text-white text-2xl bg-black/20">
+                            Who am I?
+                        </CardHeader>
+                        <CardBody className="card-body">
+                            <p>My name is Maximilian (Max) and I am a Computer Science B.S. (Concentrating in A.I.) and Mathematics B.A. candidate at Case Western Reserve University in Cleveland, OH.</p>
+                        </CardBody>
+                    </Card>
+                    <Card className="bg-black/40 h-fit">
+                        <CardHeader className="text-white text-2xl bg-black/20">
+                            Find Me
+                        </CardHeader>
+                        <CardBody className="card-body gap-3 flex flex-row justify-center">
+                            <Link href="https://github.com/Max-Schulten" target="_blank" className="social w-fit"><FontAwesomeIcon icon={faGithub} size="3x"/></Link>
+                            <Link href="" target="_blank" className="social w-fit"><FontAwesomeIcon icon={faLinkedin} size="3x"/></Link>
+                            <Link href="mailto:maximilian.schulten1@gmail.com" target="_blank" className="social w-fit"><FontAwesomeIcon icon={faEnvelope} size="3x"/></Link>
+                        </CardBody>
+                    </Card>
+                </div>
+
                 <Card className="bg-black/40 h-fit">
-                    <CardHeader className="text-white text-2xl bg-black/20">
-                        Who am I?
-                    </CardHeader>
-                    <CardBody className="card-body">
-                        <p>My name is Maximilian (Max) and I am a Computer Science B.S. (Concentrating in A.I.) and Mathematics B.A. candidate at Case Western Reserve University in Cleveland, OH.</p> 
-                    </CardBody>
-                </Card>
-                <Card className="bg-black/40">
                     <CardHeader className="text-white text-2xl bg-black/20">
                         A Short Bio
                     </CardHeader>
                     <CardBody className="card-body">
-                        I was born in Cincinnati, OH to two first generation German immigrants. I moved around a lot before high school, I spend two seperate four and two year stints respectively in Geneva, Switzerland where I attended the International School of Geneva. I also spent two year between those stints in Singapore, where I attended the Singapore American School. We eventually found our way back to Cincinnati where I completed all of high school eventually, graduating Summa Cum Laude and attending Case Western Reserve University. Here I&#39;ve begun pursuing my degress in CS and Math.
+                        I was born in Cincinnati, OH to two first generation German immigrants. I moved around a lot before high school, I spend two seperate four and two year stints respectively in Geneva, Switzerland where I attended the International School of Geneva. I also spent two years between those stints in Singapore, where I attended the Singapore American School. We eventually found our way back to Cincinnati where I completed all of high school. Eventually, graduating Summa Cum Laude and attending Case Western Reserve University. Here I&#39;ve begun pursuing my degress in CS and Math.
                     </CardBody>
                 </Card>
             </div>
-            <div className='flex flex-row justify-center mt-3'>
-                <Link><FontAwesomeIcon size="2x" icon={faGithub} /></Link>
-                <Divider className='mx-1' orientation="vertical" />
-                <Link><FontAwesomeIcon size='2x' icon={faGithub} /></Link>
-            </div>
-
         </>
     )
 }
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,9 +1,57 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { Card, CardBody, CardFooter, CardHeader, Link, Tooltip } from "@nextui-org/react"
+import { faLink } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
 function Projects() {
+
+    const [projects, setProjects] = useState([])
+
+    useEffect(() => {
+        axios.get('https://api.github.com/users/max-schulten/repos')
+            .then(response => {
+                let arr = []
+                const res = response.data
+                for (const repo of res) {
+                    if (!repo.fork && repo.name != 'Max-Schulten') {
+                        arr.push({
+                            name: repo.name,
+                            desc: repo.description,
+                            url: repo.html_url,
+                        })
+                    }
+                }
+                setProjects(arr)
+            })
+            .catch(error => {
+                console.error(error)
+            })
+    }, [])
+
     return (
         <>
             <h1 className='text-white mt-10 text-3xl text-center'>Checkout some of my projects.</h1>
             <h2 className='text-orange-400 mt-2 text-xl text-center'>These all link to github.</h2>
-
+            <div className="grid lg:grid-cols-4 gap-3 m-4 sm:grid-cols-1">
+                {projects.map((proj, index) => {
+                    return (
+                        <Card className='project-card bg-black/25' key={index}>
+                            <CardHeader className='project-card-header bg-orange-400/30 text-white'>
+                                {proj.name}
+                            </CardHeader>
+                            <CardBody className="card-body">
+                                {proj.desc}
+                            </CardBody>
+                            <Tooltip className="tooltip" content='Visit repo'>
+                                <CardFooter as={Link} href={proj.url} className="card-footer backdrop-blur-lg bg-black/50 flex justify-center">
+                                    <FontAwesomeIcon icon={faLink} size="lg" color="white"/>
+                                </CardFooter>
+                            </Tooltip>
+                        </Card>
+                    )
+                })}
+            </div>
         </>
     )
 }
