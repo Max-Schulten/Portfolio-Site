6dd29c845624c2876c8bbc38f3e60527cc28e1bb
Maximilian Schulten <maximilian.schulten1@gmail.com>
2024-07-31 13:13:02 -0400

    Adding a featured project function and a picture

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,18 @@
+import { Card, CardFooter, Image } from "@nextui-org/react"
+import max from './assets/max.jpg'
+
 function Home() {
     return (
         <>
-            <h1 className='text-white mt-56 text-3xl text-center'>Welcome to my page.</h1>
+            <h1 className='text-white mt-32 text-3xl text-center'>Welcome to my page.</h1>
             <h2 className='text-orange-400 mt-20 text-xl text-center'>Use the nav to your right to find out more about me and what I do.</h2>
+
+            <div className="flex justify-center mt-24">
+                <Card className="w-fit bg-transparent" isFooterBlurred>
+                    <Image src={max} height={400}/>
+                    <CardFooter className="justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">Thank you for visiting.</CardFooter>
+                </Card>
+            </div>
         </>
     )
 }
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,12 +1,18 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import { Card, CardBody, CardFooter, CardHeader, Link, Tooltip } from "@nextui-org/react"
-import { faLink } from "@fortawesome/free-solid-svg-icons"
+import { faLink, faRankingStar } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 function Projects() {
 
     const [projects, setProjects] = useState([])
+    const featuredProj = 'https://github.com/Max-Schulten/truth-table-plus'
+    const [feat, setFeat] = useState({})
+
+    function featured(url) {
+        return url === featuredProj
+    }
 
     useEffect(() => {
         axios.get('https://api.github.com/users/max-schulten/repos')
@@ -14,11 +20,18 @@ function Projects() {
                 let arr = []
                 const res = response.data
                 for (const repo of res) {
-                    if (!repo.fork && repo.name != 'Max-Schulten') {
+                    const bool = featured(repo.html_url)
+                    if (!repo.fork && repo.name != 'Max-Schulten' && !bool) {
                         arr.push({
                             name: repo.name,
                             desc: repo.description,
-                            url: repo.html_url,
+                            url: repo.html_url
+                        })
+                    } else if (bool) {
+                        setFeat({
+                            name: repo.name,
+                            desc: repo.description,
+                            url: repo.html_url
                         })
                     }
                 }
@@ -33,23 +46,40 @@ function Projects() {
         <>
             <h1 className='text-white mt-10 text-3xl text-center'>Checkout some of my projects.</h1>
             <h2 className='text-orange-400 mt-2 text-xl text-center'>These all link to github.</h2>
+            {featuredProj !== '' && 
+                <div className="flex justify-center m-10 text-center">
+                    <Card className="project-card bg-black/25 w-fit" isFooterBlurred>
+                        <CardHeader className="bg-orange-400/30 text-white flex justify-between">{feat.name}<div className="flex"><FontAwesomeIcon className="mx-2 mt-1" color="gold" icon={faRankingStar}/><p>Featured!</p></div></CardHeader>
+                        <CardBody className="card-body">
+                            {feat.desc}
+                        </CardBody>
+                        <Tooltip className="tooltip" content='Visit repo'>
+                                    <CardFooter as={Link} href={feat.url} target='_blank' className="flex justify-center">
+                                        <FontAwesomeIcon icon={faLink} size="lg" color="white" />
+                                    </CardFooter>
+                                </Tooltip>
+                    </Card>
+                </div>
+            }
             <div className="grid lg:grid-cols-4 gap-3 m-4 sm:grid-cols-1">
                 {projects.map((proj, index) => {
-                    return (
-                        <Card className='project-card bg-black/25' key={index}>
-                            <CardHeader className='project-card-header bg-orange-400/30 text-white'>
-                                {proj.name}
-                            </CardHeader>
-                            <CardBody className="card-body">
-                                {proj.desc}
-                            </CardBody>
-                            <Tooltip className="tooltip" content='Visit repo'>
-                                <CardFooter as={Link} href={proj.url} target='_blank' className="card-footer backdrop-blur-lg bg-black/50 flex justify-center">
-                                    <FontAwesomeIcon icon={faLink} size="lg" color="white" />
-                                </CardFooter>
-                            </Tooltip>
-                        </Card>
-                    )
+                    if (!proj.is_featured) {
+                        return (
+                            <Card className='project-card bg-black/25' key={index} isFooterBlurred>
+                                <CardHeader className='project-card-header bg-orange-400/30 text-white'>
+                                    {proj.name}
+                                </CardHeader>
+                                <CardBody className="card-body">
+                                    {proj.desc}
+                                </CardBody>
+                                <Tooltip className="tooltip" content='Visit repo'>
+                                    <CardFooter as={Link} href={proj.url} target='_blank' className="flex justify-center">
+                                        <FontAwesomeIcon icon={faLink} size="lg" color="white" />
+                                    </CardFooter>
+                                </Tooltip>
+                            </Card>
+                        )
+                    }
                 })}
             </div>
         </>
